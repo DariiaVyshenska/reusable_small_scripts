b1fbde332e3b660ece49958b732c267653a28399
Dariia Vyshenska <dariia.vyshenska@gmail.com>
2024-08-07 16:02:22 -0700

    feat: reordered columns and cleaned file name from extension

--- summarize_snp_from_rava/main.py
+++ summarize_snp_from_rava/main.py
@@ -5,6 +5,16 @@ import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def reorder_columns(summary_df):
+  determ_col = [
+    'SAMPLE_ID', 'NONSYNONYMOUS', 'SYNONYMOUS',  
+    'COMPLEX', 'STOPGAIN', 'STOPLOSS', 'TOTAL_SNPS',
+    ]
+  unmatched_columns = [col for col in summary_df.columns if col not in determ_col]
+  new_col_order = [col for col in determ_col if col in summary_df.columns] + unmatched_columns
+  
+  return summary_df[new_col_order]
+
 def summarize_snps(input_csv_path, output_csv_path):
   snp_data = pd.read_csv(input_csv_path, usecols=['Sample', 'Syn'])
   
@@ -17,8 +27,9 @@ def summarize_snps(input_csv_path, output_csv_path):
   summary_df['total_snps'] = summary_df.iloc[:, 1:].sum(axis=1)
   
   summary_df.columns = summary_df.columns.str.upper()
-  summary_df.to_csv(output_csv_path, index = False)
-  
+  summary_df['SAMPLE_ID'] = summary_df['SAMPLE_ID'].str.replace('.fastq.gz', '', regex=False)
+  ordered_df = reorder_columns(summary_df)
+  ordered_df.to_csv(output_csv_path, index = False) 
 
 def main(input_csv_path, output_path):
   try:
